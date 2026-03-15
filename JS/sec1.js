faee4540a56cc5ee002cf9f995c3c107d579443b
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2026-03-15 09:56:44 +0400

    Path To Goal

--- JS/sec1.js
+++ JS/sec1.js
@@ -1,4 +1,4 @@
 let backtoplan = document.querySelector(".backtoplan");
 backtoplan.addEventListener("click", ()=>{
-    window.location.href = "../index.html";
+    window.location.href = "/Path-To-Goal/index.html";
 });
